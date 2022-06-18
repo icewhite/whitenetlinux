2f198b17da81d944c067d499cbb76d5c7a8ba0fa
Iain White <iain.white@gmail.com>
2022-06-18 12:12:09 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ echo "Hello - welcome to whitenetlinux!"
 
 if ! command -v <the_command> &> /dev/null
 then
-    sudo apt-get install zsh
+  sudo apt-get install zsh
 elif
   echo zsh already installed
 fi
