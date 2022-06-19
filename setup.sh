d52ec893b8ea7078a11a78e91bc46ec14793724e
Iain White <iain.white@gmail.com>
2022-06-19 19:20:19 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -29,6 +29,6 @@ git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/z
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 #enable plugins
-sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions autojump zsh-syntax-highlighting)/' ./.zshrc
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ./.zshrc
 
 
