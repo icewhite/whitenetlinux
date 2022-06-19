70fd20b7c08673d250812f28ff0db8ac98bc42f1
Iain White <iain.white@gmail.com>
2022-06-19 19:20:03 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,34 @@
 echo "Hello - welcome to whitenetlinux!"
 
-if ! command -v zsh &> /dev/null
-then
+# Install zsh
+if ! type zsh &> /dev/null; then
   sudo apt-get install zsh
-elif
-  echo zsh already installed
+else
+  echo "zsh already installed"
 fi
 
+# Set zsh as default shell
+if ! [ "$SHELL" = "/bin/zsh" ]; then 
+  chsh -s /bin/zsh
+fi
+
+# oh-my-zsh
+if [ -d ~/.oh-my-zsh ]; then
+	echo "oh-my-zsh is installed"
+ else
+ 	echo "oh-my-zsh is not installed"
+  sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+fi
+
+# switch to agnoster https://github.com/agnoster/agnoster-zsh-theme
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/' ./.zshrc
+
+sudo apt-get install fonts-powerline
+
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+#enable plugins
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions autojump zsh-syntax-highlighting)/' ./.zshrc
 
 
